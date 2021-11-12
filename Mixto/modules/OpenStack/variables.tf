ced06d3944d924e769e8e3f592191197bb47e653
emp718 <93732519+emp718@users.noreply.github.com>
2021-11-12 14:19:08 +0100

    Update variables.tf

--- Mixto/modules/OpenStack/variables.tf
+++ Mixto/modules/OpenStack/variables.tf
@@ -4,10 +4,27 @@
 # * openstack_tenant_name inicializada con el nombre del proyecto en OpenStack
 # * openstack_password inicializada con la contraseña en OpenStack
 # * openstack_keypair: Nombre del archivo de claves en OpenStack
+variable "openstack_user_name" {
+    description = "The username for the Tenant."
+    default  = "emp718"
+}
+
+variable "openstack_tenant_name" {
+    description = "The name of the Tenant."
+    default  = "emp718"
+}
+
+variable "openstack_password" {
+    description = "The password for the Tenant."
+    default  = "OpenStack2627"
+}
+
+variable "openstack_keypair" {
+    description = "The keypair to be used."
+    default  = "emp718"
+}
 
 variable "openstack_auth_url" {
     description = "The endpoint url to connect to OpenStack."
     default  = "http://openstack.di.ual.es:5000/v3"
 }
-
-
