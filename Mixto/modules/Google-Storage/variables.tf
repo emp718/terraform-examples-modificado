0cfa96eea918292e4325dcb5ed7414dbfbab92aa
emp718 <93732519+emp718@users.noreply.github.com>
2021-11-12 14:04:57 +0100

    Update variables.tf

--- Mixto/modules/Google-Storage/variables.tf
+++ Mixto/modules/Google-Storage/variables.tf
@@ -1,7 +1,19 @@
 # *** YOUR CODE HERE ***
 # Definir 3 variables
 # * gcp_username inicializada con el nombre de usuario en GCP
+variable "gcp-username" {
+  description = "GCP user name"
+  default     = "emp718"
+}
 # * gcp_project inicializada con el nombre del proyecto en GCP
+variable "gcp-project" {
+  description = "GCP project"
+  default     = "My First Project"
+}
 # * gcp_bucket_name inicializada con el nombre del bucket a crear en Google Storage
+variable "gcp-bucket" {
+  description = "GCP bucket"
+  default     = "bucket-emp718"
+}
 # **********************
 
