37578aec75eb2787c28fede6df2bebe7c9652535
emp718 <93732519+emp718@users.noreply.github.com>
2021-11-12 14:26:40 +0100

    Update providers.tf

--- Mixto/modules/OpenStack/providers.tf
+++ Mixto/modules/OpenStack/providers.tf
@@ -5,6 +5,6 @@ provider "openstack" {
 # **********************
   user_name   = var.openstack_user_name
   tenant_name = var.openstack_tenant_name
-  password    = var.openstack_password
+  password    = var.PASSWORD
   auth_url    = var.openstack_auth_url
 }
